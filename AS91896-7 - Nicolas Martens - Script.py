318191b3de95e97e87b714c57f12f2ba879f6180
CorruptedBrainn <102107009+CorruptedBrainn@users.noreply.github.com>
2024-06-21 10:20:49 +1200

    Update AS91896-7 - Nicolas Martens - Script.py

---
+++ AS91896-7 - Nicolas Martens - Script.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import ttk
+
+def quit():
+    # For GUI
+
+def main():
+    while 1:
+        break
+
+if __name__ == "__main__":
+    main():
